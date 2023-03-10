0e78662d15b82cf66f61e5bbeb7872aafac6273f
Mitchellzhou1 <mitche.zhou1@gmail.com>
2023-03-10 10:24:12 -0500

    added cool fight scenes

---
+++ Assets/SharedScripts/FightZombie.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class FightZombie : MonoBehaviour
+{
+    
+    NavMeshAgent _navMeshAgent;
+    GameObject player;
+    public float milliseconds;
+
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("fight");
+        StartCoroutine(ChasePlayer());
+    }
+
+    IEnumerator ChasePlayer(){
+        while (true){
+            yield return new WaitForSeconds(milliseconds);
+            _navMeshAgent.destination = player.transform.position;
+        }
+    }
+
+}
